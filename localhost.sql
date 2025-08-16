c981662410e8630fee71b44a1045c6b8a74af259
Allen Lu <168053902+Darkmatter126ms@users.noreply.github.com>
2025-08-16 16:35:01 +0800

    Add files via upload

---
+++ localhost.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 16, 2025 at 08:32 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `music`
+--
+CREATE DATABASE IF NOT EXISTS `music` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `music`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `album_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `artist_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`album_id`, `title`, `artist_id`) VALUES
+(1, 'Innuendo', 1),
+(2, 'Made In Heaven', 1),
+(3, 'The 1st Album', 2),
+(4, 'Lets Talk About Love', 2),
+(5, 'Showcase', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artist`
+--
+
+CREATE TABLE `artist` (
+  `artist_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `artist`
+--
+
+INSERT INTO `artist` (`artist_id`, `name`) VALUES
+(1, 'Queen'),
+(2, 'Modern Talking'),
+(3, 'Patsy Cline');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `genre_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`genre_id`, `name`) VALUES
+(1, 'Classics'),
+(2, 'Pop');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `track`
+--
+
+CREATE TABLE `track` (
+  `track_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `len` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  `album_id` int(11) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `track`
+--
+
+INSERT INTO `track` (`track_id`, `title`, `len`, `rating`, `count`, `album_id`, `genre_id`) VALUES
+(1, 'Bohemian Rhapsody', 372, 5, 0, 2, 1),
+(2, 'I am going slightly mad', 182, 5, 0, 1, 1),
+(3, 'Headlong', 190, 5, 0, 1, 1),
+(4, 'Ride the wild wind', 255, 4, 0, 1, 1),
+(5, 'Dont try too hard', 250, 4, 0, 1, 1),
+(6, 'Let me live', 182, 5, 0, 2, 1),
+(7, 'Mother Love', 333, 5, 0, 2, 2),
+(8, 'My life has been sad', 182, 2, 0, 2, 1),
+(9, 'I was born to love you', 190, 5, 0, 2, 1),
+(10, 'Heaven for everyone', 400, 1, 0, 2, 1),
+(11, 'Too much love will kill me', 192, 5, 0, 2, 1),
+(12, 'You can win if you want', 222, 5, 0, 3, 2),
+(13, 'You\'re my heart, my soul', 390, 5, 0, 3, 2),
+(14, 'There\'s too much blue', 181, 5, 0, 3, 2),
+(15, 'One in a million', 120, 4, 0, 4, 2),
+(16, 'Lucky Guy', 202, 4, 0, 4, 2),
+(17, 'Bells of Paris', 190, 5, 0, 4, 2),
+(18, 'I fall to pieces', 500, 1, 0, 5, 1),
+(19, 'Crazy', 190, 5, 0, 5, 1),
+(20, 'Have you ever been lonely', 182, 5, 0, 5, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`album_id`),
+  ADD KEY `title` (`title`) USING BTREE,
+  ADD KEY `artist_id` (`artist_id`);
+
+--
+-- Indexes for table `artist`
+--
+ALTER TABLE `artist`
+  ADD PRIMARY KEY (`artist_id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
+-- Indexes for table `track`
+--
+ALTER TABLE `track`
+  ADD PRIMARY KEY (`track_id`),
+  ADD KEY `title` (`title`) USING BTREE,
+  ADD KEY `album_id` (`album_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `artist`
+--
+ALTER TABLE `artist`
+  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `track`
+--
+ALTER TABLE `track`
+  MODIFY `track_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `track`
+--
+ALTER TABLE `track`
+  ADD CONSTRAINT `track_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `track_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`genre_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
